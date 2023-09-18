38bf3a9f01f824947fa609e26fb246a0e0be651d
J0hn316 <jcordell316@gmail.com>
2023-09-18 14:27:43 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get("/", function (req, res) {
   res.send("Welcome to the Pokemon App!");
 });
 
-app.get("/pokemon", function (req, res) {
-  res.send(pokemon);
+app.get("/pokemon/", function (req, res) {
+  res.render("Index", { pokemon: pokemon });
 });
 
 app.listen(port, () => {
---
+++ views/Show.jsx
@@ -0,0 +1,9 @@
+const React = require("react");
+
+class Show extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+module.exports = Show;
